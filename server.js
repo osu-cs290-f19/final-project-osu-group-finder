0abf97ef0234e26ff96557ea3e83026520ab10da
cranberriez <jakevilevac@gmail.com>
2019-12-09 18:00:09 -0800

    update server.js to use handlebars

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var exphbs = require("express-handlebars")
+var exphbs = require("express-handlebars");
 var app = express();
 var port = process.env.PORT || 3000;
-var posts = require("/public/posts.json")
-var users = require("public/users.json")
 
-app.engine('handlebars', exphbs())
-app.set('view engine', 'handlebars')
+//// needs to be replaced with an actual database
+var posts = require("public/posts.json");
+var users = require("public/users.json");
+////
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
@@ -16,16 +19,17 @@ app.get('/', function (req, res) {
 
 app.get('/posts/:post', function (req, res) {
   var postID = req.params.post;
-  if (postID <= posts.length) {
+  if (postID >= 0 && postID < posts.length) {
     res.status(200).render('body', {post: posts[postID]})
   }
   else {
-    res.status(404).render('404')
+    res.status(404).render('404');
   }
 });
 
 app.get('*', function (req, res) {
-  res.status(404).render('404')
+  // res.status(404).render('404')
+  res.status(404).sendFile(__dirname + "/public/404.html");
 });
 
 app.listen(port, function () {
