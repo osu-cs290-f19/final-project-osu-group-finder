144cdf95f16839623b1b6d1f962c6918afb74432
cranberriez <jakevilevac@gmail.com>
2019-12-10 02:40:00 -0800

    finished setting up mongodb

--- server.js
+++ server.js
@@ -1,52 +1,109 @@
-var express = require('express');
-var exphbs = require("express-handlebars");
-var app = express();
-var port = process.env.PORT || 3000;
+const path = require('path');
+const express = require('express');
+const exphbs = require("express-handlebars");
+const BodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
+const port = process.env.PORT || 3000;
 
-//// needs to be replaced with an actual database
-var posts = require("./public/posts.json");
-var users = require("./public/users.json");
-////
+const DB_URL = "mongodb+srv://<username>:<password>@cluster0-z3fba.mongodb.net/test?retryWrites=true&w=majority";
+const DATABASE_NAME = "database0";
+var database, userCOLL, postCOLL;
 
-app.engine('handlebars', exphbs());
+const app = express();
+var hbs = exphbs.create({
+    defaultLayout: 'main',
+
+    helpers: {
+        formatDate: function(timestamp) {
+            var date = new Date(timestamp * 1000);
+
+            var monthNames = [
+                "Jan", "Feb", "Mar",
+                "Apr", "May", "June", "July",
+                "Aug", "Sept", "Oct",
+                "Nov", "Dec"
+              ];
+            
+              var day = date.getDate();
+              var monthIndex = date.getMonth();
+              var year = date.getFullYear();
+            
+              return monthNames[monthIndex] + " " + day;
+        }
+    }
+});
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.status(200).render('body', {posts: posts});
+    postCOLL.find({}).toArray((error, result) => {
+        if(error) {
+            return res.status(500).send(error);
+        }
+        res.status(200).render('body', {posts: result});
+    });
 });
 
-app.get('/posts/:post', function (req, res) {
-  var postID = req.params.post;
-  if (postID >= 0 && postID < posts.length) {
-    res.status(200).render('body', {post: posts[postID]})
-  }
-  else {
-    res.status(404).render('404');
-  }
+app.get('/posts/:postId', function (req, res) {
+    var postID = req.params.postId;
+    postCOLL.find({_id : postID}).toArray((error, result) => {
+        console.log("sending posts id " + postID)
+        res.status(200).render('body', {posts: result});
+    });
 });
 
-app.post('/user', function (req, res) {
-  res.status(200).send("User Added")
+app.post('/addPost', function (req, res) {
+    collection.insert(request.body, (error, result) => {
+        if(error) {
+            return res.status(500).send(error);
+        }
+        res.status(200)
+    });
 });
 
-app.post('/user', function (req, res) {
-  res.status(200).send("Post Created")
+app.delete("/posts/:postId", function (req, res) {
+    var postId = req.params.postId;
+    if (postId <= posts.length) {
+            res.status(200).send("Deleted Post " + postId)
+    }
+    else {
+            next();
+    }
 });
 
-app.delete("/user", function (req, res) {
-  res.status(200).send("Deleted User")
+app.get('/users/:user', function (req, res) {
+    res.status(200).send("User Added")
 });
 
-app.delete("/post", function (req, res) {
-  res.status(200).send("Deleted Post")
+app.post('/addUser', function (req, res) {
+    res.status(200).send("User Added")
 });
 
-app.get("*", function (req, res) {
-  res.status(404).render('404')
+app.delete("/delUser/:username", function (req, res) {
+    var user = req.body
+    userCOLL.deleteOne(
+        { username: user.username, posts: user.posts, name: user.name } // specifies the document to delete
+    )
+});
+
+app.get('*', function (req, res) {
+    res.status(404).render('404')
 });
 
 app.listen(port, function () {
-  console.log("== Server is listening on port", port);
+    console.log("== Server is listening on port", port);
+    MongoClient.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        userCOLL = database.collection("users");
+        postCOLL = database.collection("posts");
+        console.log("== Connected to '" + DATABASE_NAME + "'");
+    });
 });
